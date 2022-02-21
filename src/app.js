b25007921cdda3cf29acf1862696406c757fef1c
Talip <talipakcelik@gmail.com>
2022-02-22 01:04:44 +0300

    Add fill renderWeather function with resolved data properties

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 const weatherInput = document.querySelector('#weather-query');
+const weatherContainer = document.querySelector('.weather-container');
+const location = document.querySelector('.location');
+const temp = document.querySelector('.temp');
+const icon = document.querySelector('.icon');
+const description = document.querySelector('.description');
+const humidity = document.querySelector('.humidity');
+const visibility = document.querySelector('.visibility');
+const maxTemp = document.querySelector('.max-temp');
 
 function getWeather() {
   fetch(
@@ -22,16 +30,23 @@ async function getWeatherAsync() {
   console.log(data);
   console.log(data.main);
   // weatherData = data.main.temp;
-  renderWeather(data.main.temp);
+  renderWeather(data);
+  console.log(data.weather[0].icon);
 }
 
 function renderWeather(data) {
-  const weatherContainer = document.createElement('div');
-  const location = document.createElement('p');
+  weatherContainer.style.display = '';
+  location.textContent = `${data.name}, ${data.sys.country}`;
+  temp.textContent = `${parseInt(data.main.temp)}°C`;
+  description.textContent = `Feels like ${parseInt(data.main.feels_like)}°C, ${
+    data.weather[0].description
+  }.`;
+  icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
-  document.querySelector('main').append(weatherContainer);
-  weatherContainer.append();
-  console.log(data);
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  visibility.textContent = `Visibility: ${String(data.visibility).charAt(
+    0
+  )}${String(data.visibility).charAt(1)} km`;
 }
 
 // renderWeather();
