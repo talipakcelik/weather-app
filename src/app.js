9e9bc7ea9481698a7bb40d26cabd218731d02782
Talip <talipakcelik@gmail.com>
2022-02-22 02:27:23 +0300

    Add error handling with try and catch

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const weatherContainer = document.querySelector('.weather-container');
 const location = document.querySelector('.location');
 const temp = document.querySelector('.temp');
 const icon = document.querySelector('.icon');
+const descriptionContainer = document.querySelector('.description-container');
 const description = document.querySelector('.description');
 const humidity = document.querySelector('.humidity');
 const visibility = document.querySelector('.visibility');
@@ -21,21 +22,35 @@ function getWeather() {
 }
 
 async function getWeatherAsync() {
-  let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${weatherInput.value}&units=metric&appid=027a1065ba3125e88ef0663d3b8231e9`
-  );
+  try {
+    let response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${weatherInput.value}&units=metric&appid=027a1065ba3125e88ef0663d3b8231e9`
+    );
 
-  let data = await response.json();
+    let data = await response.json();
 
-  console.log(data);
-  console.log(data.main);
-  // weatherData = data.main.temp;
-  renderWeather(data);
-  console.log(data.weather[0].icon);
+    console.log(data);
+
+    console.log(data.cod);
+    if (data.cod === '404') {
+      weatherContainer.style.display = '';
+      location.textContent = data.message;
+      icon.style.display = 'none';
+      descriptionContainer.style.display = 'none';
+      temp.style.display = 'none';
+    } else {
+      renderWeather(data);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 function renderWeather(data) {
   weatherContainer.style.display = '';
+  icon.style.display = '';
+  descriptionContainer.style.display = '';
+  temp.style.display = '';
   location.textContent = `${data.name}, ${data.sys.country}`;
   temp.textContent = `${parseInt(data.main.temp)}°C`;
   description.textContent = `Feels like ${parseInt(data.main.feels_like)}°C, ${
